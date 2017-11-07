59f2c072328609c00f7475e77f2a00278fd0ffc6
Shawn David Vella <b43358@mcast.org.mt>
2017-11-07 12:28:40 +0100

    - Handled LoseBorder collision. - Tested Mouse Position.

---
+++ Block Breaker 4.2C/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+    static MusicPlayerScript instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            print("Duplicate Music Player is self-destructing!");
+            Destroy(gameObject);
+        }
+        else // instance is still null so this is the first Music player
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Block Breaker 4.2C/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Input.mousePosition);
+
+        Vector3 newPaddlePos = new Vector3(Input.mousePosition.x,
+            this.transform.position.y, this.transform.position.z);
+
+        this.transform.position = newPaddlePos;
+	}
+}
